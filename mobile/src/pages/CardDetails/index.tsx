5d141297e0877141c0b81a447caa1fd0c2b47bf0
Fabiopf02 <fabiofranca593@gmail.com>
2022-01-08 18:54:30 -0300

    verificar se as cartelas já foram geradas

--- mobile/src/pages/CardDetails/index.tsx
+++ mobile/src/pages/CardDetails/index.tsx
@@ -1,14 +1,34 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {NavigationProps} from '../../types/types';
 import {ThemeContext} from 'styled-components';
 import {formatDate} from '../../utils/format';
 import {Container, Title, Text, Rect, Row} from './styles';
 
 import Button from '../../components/Button';
+import {api} from '../../services/api';
+
+interface IResponse {
+  cardsExists: boolean;
+}
 
 const CardDetails: React.FC<NavigationProps<'CardDetails'>> = ({route}) => {
   const {colors} = useContext(ThemeContext);
+  const [cardsExists, setCardsExists] = useState(true);
   const cardList = route.params;
+
+  useEffect(() => {
+    async function checkIfCardsExists() {
+      try {
+        const response = await api.get<IResponse>('/cards_exists', {
+          headers: {
+            listId: cardList._id,
+          },
+        });
+        setCardsExists(response.data.cardsExists);
+      } catch {}
+    }
+    checkIfCardsExists();
+  }, [cardList]);
 
   return (
     <Container>
@@ -27,8 +47,16 @@ const CardDetails: React.FC<NavigationProps<'CardDetails'>> = ({route}) => {
       <Text>Criado em: {formatDate(cardList.createdAt)}</Text>
       <Text>Atualizado em: {formatDate(cardList.createdAt)}</Text>
 
-      <Button text="Gerar Cartelas" bg={colors.primary} />
-      <Button text="Baixar Cartelas" bg={colors.secondary} disabled={true} />
+      <Button
+        text="Gerar Cartelas"
+        disabled={cardsExists}
+        bg={colors.primary}
+      />
+      <Button
+        text="Baixar Cartelas"
+        bg={colors.secondary}
+        disabled={!cardsExists}
+      />
     </Container>
   );
 };
